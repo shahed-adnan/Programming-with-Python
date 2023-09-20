1980b1969b073aa9e123ad6b38c30a08936f5368
shahed-adnan <shahedcse.nu@gmail.com>
2023-09-20 22:52:35 +0200

    feat: reading values from console

--- Variables.py
+++ Variables.py
@@ -1,3 +1,7 @@
 # declaring variable and assigning value to variable
 price = 100
 print(price)
+
+# reading input from console and assign to variable
+course_name = input('Enter a course name: ')
+print(course_name)
