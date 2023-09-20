f6e67bf47a2bd72293d430052c70ab71ce618f80
shahed-adnan <shahedcse.nu@gmail.com>
2023-09-20 23:00:47 +0200

    feat: converting input value to different datatypes

--- Variables.py
+++ Variables.py
@@ -5,3 +5,11 @@ print(price)
 # reading input from console and assign to variable
 course_name = input('Enter a course name: ')
 print(course_name)
+
+# reading input from console and convert to int type to perform arithmatic
+house_price = input('Enter the house price in numeric value: ')
+print(int(house_price))
+
+# convert to float
+house_price = input('Enter a decimal value for house price: ')
+print(float(house_price))
