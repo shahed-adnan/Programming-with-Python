90a67ef6092bc01318c1e2a7dca711de77df55c5
shahed-adnan <shahedcse.nu@gmail.com>
2023-11-03 01:24:47 +0100

    feat: functions with multiple arguments or parameter

--- Functions.py
+++ Functions.py
@@ -4,5 +4,11 @@ def add_Three(input_var):
     return output_var
 
 
+# function with multiple arguments
+def total_marks(english, math, physics):
+    return english + math + physics
+
+
 new_number = add_Three(10)
 print(new_number)
+print(total_marks(85, 100, 95))
